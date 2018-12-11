c8e80010c42ba8266b850b4d2b2b6c90a48d54f1
Duncan Finney <duncan.finney@gmail.com>
2018-12-11 07:09:22 -0500

    Day 06 was actually right. There was a bug in the input generator! :)

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,18 @@
 //mod day03;
 //mod day04;
 //mod day05;
-//mod day06;
+mod day06;
 //mod day07;
 //mod day08;
 //mod day09;
 //mod day10;
-mod day11;
+//mod day11;
 
 fn main() {
     use std::time::Instant;
     let start_time = Instant::now();
 
-    day11::solve();
+    day06::solve();
 
     let duration = start_time.elapsed().as_millis();
     println!("runtime: {}ms", duration);
