5252a02e3085c80dc774c6823f79d0f784da5a51
Duncan Finney <duncan.finney@gmail.com>
2018-12-11 07:01:27 -0500

    Day 11 done

--- src/day11.rs
+++ src/day11.rs
@@ -1,3 +1,5 @@
+use rayon::prelude::*;
+
 pub fn solve() {
     let input = 2866;
 
@@ -9,8 +11,12 @@ pub fn solve() {
 }
 
 fn part_one(input: &i32) -> FuelCell {
-    let mut board = (0..=300)
-        .map(|y| (0..=300).map(|x| FuelCell::new(x, y, *input)).collect::<Vec<_>>())
+    let board = (0..=300)
+        .map(|y| {
+            (0..=300)
+                .map(|x| FuelCell::new(x, y, *input))
+                .collect::<Vec<_>>()
+        })
         .collect::<Vec<_>>();
     let total_power = |x: i32, y: i32| -> i32 {
         if x >= 299 || y >= 299 {
@@ -35,11 +41,50 @@ fn part_one(input: &i32) -> FuelCell {
 }
 
 // TODO
-fn part_two(input: &i32) -> () {
-    let mut board = (0..=300)
-        .map(|y| (0..=300).map(|x| FuelCell::new(x,y, *input)).collect::<Vec<_>>())
+fn part_two(input: &i32) -> (i32, i32, i32) {
+    let board = (0..=300)
+        .map(|y| {
+            (0..=300)
+                .map(|x| FuelCell::new(x, y, *input))
+                .collect::<Vec<_>>()
+        })
         .collect::<Vec<_>>();
 
+    let total_power = |x: i32, y: i32, size: i32| -> i32 {
+        if size == 0 {
+            return 0;
+        }
+        if x > 300 - size || y > 300 - size {
+            return 0;
+        }
+
+        let mut ret = 0;
+        for y in y..=y + (size - 1) {
+            for x in x..=x + (size - 1) {
+                ret += board[y as usize][x as usize].power_level;
+            }
+        }
+        ret
+    };
+
+    // you can solve this with dynamic programming...
+    // but I'm brute forcing the shit out of it.
+    let largest_square = (0..300i32)
+        .into_par_iter()
+        .map(|size| {
+            board
+                .iter()
+                .flatten()
+                .max_by_key(|fc| total_power(fc.x, fc.y, size))
+                .unwrap()
+                .clone()
+        })
+        .enumerate()
+        .max_by_key(|pair| total_power(pair.1.x, pair.1.y, pair.0 as i32))
+        .unwrap();
+
+
+    (largest_square.1.x, largest_square.1.y, largest_square.0 as i32)
 }
 
 #[derive(Debug, Clone, PartialOrd, PartialEq)]
@@ -95,15 +140,16 @@ mod tests {
 
         for example in examples.iter() {
             let (point, grid_serial_num, expected_power_level) = example;
-            let point = FuelCell::new(x,y,grid_serial_num);
+            let point = FuelCell::new(point.0, point.1, *grid_serial_num);
             let power_level = point.power_level;
             assert_eq!(power_level, *expected_power_level);
         }
     }
 
     #[test]
-    fn part_one_example() {
+    fn part_two_example() {
         let input = 18;
+        let part_two = part_two(&input);
+        assert_eq!(part_two,(90,269,16));
     }
-
 }
