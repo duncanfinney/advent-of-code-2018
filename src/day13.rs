81a5ccf2cc5e9f61aa1fe2aa06a25f171b008cfb
Duncan Finney <duncan.finney@gmail.com>
2018-12-14 10:34:12 -0500

    Parsing the board

--- src/day13.rs
+++ src/day13.rs
@@ -2,22 +2,36 @@ use std::collections::HashMap;
 
 pub fn solve() {
     let input = include_str!("../input/day13");
+    let input = parse_input(&input);
+
+    input.debug_print();
+
 }
 
 
-fn parse_input(input :&str) {
+fn parse_input(input :&str) -> Board {
+    let mut board = Board::default();
     input
         .lines()
         .enumerate()
         .for_each(|l| {
-            let y = l.0;
-            l.1.chars().enumerate().for_each(|x| match x.1 {
-
+            let y = l.0 as u32;
+            l.1.chars().enumerate().for_each(|pair| {
+                let x = pair.0 as u32;
+                let tile_type = match pair.1 {
+                    '/' => TileType::DiagonalForward,
+                    '\\' => TileType::DiagonalBackward,
+                    '|' => TileType::UpDown,
+                    '-' => TileType::LeftRight,
+                    _ => TileType::Empty
+                };
+                board.set_tile(&Location{x,y}, tile_type);
             });
         });
+    board
 }
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 struct Board {
     tiles: HashMap<Location, TileType>,
 
@@ -30,25 +44,30 @@ impl Board {
         self.tiles.get(l).unwrap_or(&TileType::Empty)
     }
 
-    fn set_tile(&mut self, l: &Location, t: &TileType) {
-        self.tiles.insert(l.clone(), *t.clone());
+    fn set_tile(&mut self, l: &Location, t: TileType) {
+        if l.x > self.max_x{
+            self.max_x = l.x;
+        }
+        if l.y > self.max_y {
+            self.max_y = l.y
+        }
+        self.tiles.insert(l.clone(), t);
     }
 
     fn debug_print(&self) {
-        use TileType::*;
-        for x in 0..self.max_x {
-            for y in 0..self.max_y {
+        for y in 0..=self.max_y {
+            for x in 0..=self.max_x {
                 let symbol = match self.get_tile(&Location{ x, y }) {
-                    Empty => " ",
-                    UpDown => "|",
-                    LeftRight => "-",
-                    DiagonalForward => "/",
-                    DiagonalBackward => r"\",
+                    TileType::Empty => " ",
+                    TileType::UpDown => "|",
+                    TileType::LeftRight => "-",
+                    TileType::DiagonalForward => "/",
+                    TileType::DiagonalBackward => r"\",
 
                 };
-                print!(symbol);
+                print!("{}", symbol);
             }
-            print("\n");
+            print!("\n");
         }
     }
 }
