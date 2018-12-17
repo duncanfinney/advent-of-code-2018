06f87ccbbe88cef7c4f02114e62525126a6a1556
Duncan Finney <duncan.finney@gmail.com>
2018-12-17 07:16:28 -0500

    Day 12 also not working

--- src/day12.rs
+++ src/day12.rs
@@ -2,7 +2,22 @@ use std::collections::HashMap;
 
 pub fn solve() {
     let input = include_str!("../input/day12");
-    let input = parse_input(&input);
+    let mut input = parse_input(&input);
+
+    print!("0: ");
+    input.debug_print();
+    for n in 0..20 {
+        print!("{}: ", n);
+        println!("min, max = {}, {}", input.min, input.max);
+        input.do_generation();
+        input.debug_print();
+    }
+
+    let mut sum = 0;
+    for (n, plant_state) in input.pot_states.iter().filter(|p| p.1 == &PotState::Pot) {
+        sum += n;
+    }
+    println!("part_one= {}", sum);
 }
 
 fn parse_input(input: &str) -> Pots {
@@ -12,38 +27,137 @@ fn parse_input(input: &str) -> Pots {
     let pot_states = split.next().unwrap().trim();
     let pot_states = pot_states
         .chars()
-        .map(|c| match c {
-            '#' => PotState::Pot,
-            '.' => PotState::Empty,
-            _ => panic!("bad input"),
-        })
+        .map(PotState::from)
         .enumerate()
         .map(|(size, ps)| (size as i32, ps))
         .collect();
 
-    println!("pot_state= {:?}", pot_states);
-    Pots { pot_states }
+    //burn a line
+    lines.next();
+
+    //repacements
+    let replacements = lines
+        .map(|l| {
+            let mut split = l.split(" => ");
+            let old = split.next().unwrap();
+            let old = old.chars().map(PotState::from).collect::<Vec<_>>();
+
+            let new = split.next().unwrap().chars().next().unwrap();
+            let new = PotState::from(new);
+            Replacement { old, new }
+        })
+        .collect::<Vec<_>>();
+
+    Pots::new(pot_states, replacements)
 }
 
 #[derive(Debug)]
 struct Pots {
     pot_states: HashMap<i32, PotState>,
+    replacements: Vec<Replacement>,
+    min: i32,
+    max: i32,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Copy, Clone)]
 enum PotState {
     Pot,
     Empty,
 }
 
+impl From<char> for PotState {
+    fn from(c: char) -> Self {
+        match c {
+            '#' => PotState::Pot,
+            '.' => PotState::Empty,
+            _ => panic!("bad input"),
+        }
+    }
+}
+
 #[derive(Debug)]
 struct Replacement {
     old: Vec<PotState>,
-    new: Vec<PotState>,
+    new: PotState,
 }
 
 impl Pots {
+    fn new(pots: HashMap<i32, PotState>, replacements: Vec<Replacement>) -> Self {
+        let min = pots.iter().map(|p| p.0).min().unwrap().to_owned();
+        let max = pots.iter().map(|p| p.0).max().unwrap().to_owned();
+
+        Pots {
+            pot_states: pots,
+            replacements,
+            min,
+            max,
+        }
+    }
+
     fn get(&self, n: i32) -> &PotState {
         self.pot_states.get(&n).unwrap_or(&PotState::Empty)
+    }
+
+    fn set(&mut self, n: i32, p: PotState) {
+        self.pot_states.insert(n, p);
+        if n > self.max {
+            self.max = n;
+        }
+        if n < self.min {
+            self.min = n;
+        }
+    }
+
+    fn do_generation(&mut self) {
+        let mut new_pots = HashMap::new();
+        let mut min = 0;
+        let mut max = 0;
+        for pot_n in self.min-2..=self.max+2 {
+            let pattern = vec![
+                *self.get(pot_n - 2),
+                *self.get(pot_n - 1),
+                *self.get(pot_n),
+                *self.get(pot_n + 1),
+                *self.get(pot_n + 2),
+            ];
+            let new_state = match self.replacements.iter().find(|r| r.old.eq(&pattern)) {
+                Some(r) => r.new,
+                None => pattern[2],
+            };
+            if self.replacements.iter().find(|r| r.old.eq(&pattern)).is_some() {
+             println!("found a replacment at least");
+            }
+
+            new_pots.insert(pot_n, new_state);
+            if pot_n < min {
+                min = pot_n
+            }
+            if pot_n > max {
+                max = pot_n;
+            }
+        }
+        self.pot_states = new_pots;
+        self.min = min;
+        self.max = max;
+    }
+
+    fn debug_print(&self) {
+//        let dbg_str :String = self
+//            .pot_states
+//            .iter()
+//            .map(|p| match p.1 {
+//                PotState::Pot => "#",
+//                PotState::Empty => ".",
+//            })
+//            .collect();
+        for n in -2..30 {
+            let c = match self.pot_states.get(&n) {
+              Some(x) if x == &PotState::Pot => "#",
+               _ => "."
+           };
+           print!("{}", c)
+        }
+        print!("\n");
+//        println!("{}", dbg_str);
     }
 }
